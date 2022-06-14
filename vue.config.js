c1e910b6f960eb4f9f2b6aca397435ff53ab45da
DylanGiesberts <dylan.giesberts@gmail.com>
2022-06-14 12:25:07 +0200

    Testing

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: config => {
-    config.output.filename = '[name].js';
-  }
+  // configureWebpack: config => {
+  //   config.output.filename = '[name].js';
+  // }
 })
