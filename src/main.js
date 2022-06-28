5377f760f578ff5f1690543617d5bd529e26af5d
DylanGiesberts <dylan.giesberts@gmail.com>
2022-06-28 11:41:38 +0200

    Added loading spinner as a component

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import { defineCustomElement } from 'vue';
 
 import { default as badgeButton } from './components/BadgeButton.ce.vue';
 import { default as starRating } from './components/StarRating.ce.vue';
+import { default as loadingSpinner } from './components/LoadingSpinner.ce.vue';
 
 const BadgeButton = defineCustomElement(badgeButton);
 const StarRating = defineCustomElement(starRating);
+const LoadingSpinner = defineCustomElement(loadingSpinner);
 
 customElements.define('star-rating', StarRating);
 customElements.define('badge-button', BadgeButton);
+customElements.define('loading-spinner', LoadingSpinner);
